6a9daa77faddcd9b9f5f8aff46555e993a763909
Siddharth Das <sidz.143@gmail.com>
2019-09-10 13:41:00 +0530

    Creating UDF and using %sql

--- notebooks/practice-02.scala
+++ notebooks/practice-02.scala
@@ -1,1 +1,50 @@
 // Databricks notebook source
+// MAGIC %md
+// MAGIC #Learning to load data from dls to adb
+// MAGIC 
+// MAGIC Processing the data
+
+// COMMAND ----------
+
+case class Customer (customerid: Int, fullname: String, address: String, credit: Int, status: Boolean, remarks: String)
+
+val customersCsvLocation = "/mnt/data/customers/*.csv"
+val customers = 
+  spark
+    .read
+    .option("inferSchema", true)
+    .option("header", true)
+    .option("sep", ",")
+    .csv(customersCsvLocation)
+    .as[Customer]
+    
+customers.createOrReplaceTempView("customers")
+
+// COMMAND ----------
+
+// MAGIC %sql
+// MAGIC 
+// MAGIC SELECT address AS CustomerLocation, COUNT(*) AS NoOfCustomers FROM customers
+// MAGIC GROUP BY address
+// MAGIC ORDER BY address
+
+// COMMAND ----------
+
+val getCustomerType = (credit: Int) => {
+  if(credit < 10000) "Silver"
+  else if(credit >= 10000 && credit < 25000) "Gold"
+  else "Platinum"
+}
+
+spark.udf.register("getCustomerType", getCustomerType)
+
+// COMMAND ----------
+
+// MAGIC %sql
+// MAGIC SELECT getCustomerType(credit) AS CustomerType, COUNT(*) AS NoOfCustomers
+// MAGIC FROM customers
+// MAGIC GROUP BY CustomerType
+// MAGIC ORDER BY CustomerType
+
+// COMMAND ----------
+
